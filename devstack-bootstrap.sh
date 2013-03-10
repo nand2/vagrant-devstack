b42dbea7f2fe59c0979d03bf7f88e17ce97a5546
Nicolas Deschildre <ndeschildre@gmail.com>
2013-03-10 12:05:26 +0100

    First try.

--- devstack-bootstrap.sh
+++ devstack-bootstrap.sh
@@ -2,7 +2,21 @@
 
 set -e
 
-apt-get update
-apt-get install -qqy git
+# Make apt faster!
+sudo sed -i 's/http:\/\/us.archive.ubuntu.com\/ubuntu\//mirror:\/\/mirrors.ubuntu.com\/mirrors.txt/g' /etc/apt/sources.list
+
+sudo apt-get update
+sudo apt-get install -qqy git
+
+# Fetch ourselves
+git clone https://github.com/nand2/vagrant-devstack.git
+
+# Fetch devstack
 git clone https://github.com/openstack-dev/devstack.git
 cd devstack
+
+# Copy conf to be used, todo: make that overridable
+cp ~/vagrant-devstack/devstack.conf localrc
+
+# Launch devstack!
+yes '' | ./stack.sh

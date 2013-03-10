0fb36e560f00ff9e85893e47c494b14b89fb9557
Nicolas Deschildre <ndeschildre@gmail.com>
2013-03-10 12:18:15 +0100

    That should make it work.

--- devstack-bootstrap.sh
+++ devstack-bootstrap.sh
@@ -13,10 +13,10 @@ git clone https://github.com/nand2/vagrant-devstack.git
 
 # Fetch devstack
 git clone https://github.com/openstack-dev/devstack.git
-cd devstack
 
 # Copy conf to be used, todo: make that overridable
 cp ~/vagrant-devstack/devstack.conf localrc
 
 # Launch devstack!
+cd devstack
 yes '' | ./stack.sh
